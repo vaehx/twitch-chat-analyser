3df6c8397bc8e5314911a64b5f900bb435c1d7b1
vaehx <prosicraft@googlemail.com>
2017-12-11 21:10:00 +0100

    Write message into MySQL for now

--- Bot/src/main/java/de/prkz/TwitchBot/Main.java
+++ Bot/src/main/java/de/prkz/TwitchBot/Main.java
@@ -16,12 +16,19 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 
+		if (args.length < 1) {
+			System.err.println("Parameters: <channel>");
+			return;
+		}
+
+		String channel = args[0];
+
 		// Setup twitch IRC bot
 		Configuration config = new Configuration.Builder()
 				.setName("justinfan618723")
 				.addServer("irc.chat.twitch.tv", 6667)
 				.addListener(new Bot("docker-host:9092"))
-				.addAutoJoinChannel("#food")
+				.addAutoJoinChannel("#" + channel)
 				.buildConfiguration();
 
 		bot = new PircBotX(config);
