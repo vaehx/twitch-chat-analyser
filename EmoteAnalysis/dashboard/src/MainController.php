546106667b1de0f0ecfc140111f372222795e945
vaehx <prosicraft@googlemail.com>
2018-07-02 17:42:28 +0200

    Add user leaderboard, only show last n minutes, ...

--- EmoteAnalysis/dashboard/src/MainController.php
+++ EmoteAnalysis/dashboard/src/MainController.php
@@ -13,20 +13,30 @@ class MainController implements ControllerProviderInterface
 		$route = $app['controllers_factory'];
 		$db = $app['db'];
 
-		$route->get('/', function() use($app, $db) {
+		/**
+		 * Emote statistics overview
+		 */
+		$route->get('/', function(Request $request) use($app, $db) {
 			$stmt = $db->query("SELECT DISTINCT emote FROM emotes");
 			$emotes = [];
 			while ($row = $stmt->fetch())
 				$emotes[] = $row[0];
 
-			$mlemStats = self::getEmoteStatistics('moon2MLEM', $db);
-			$smileStats = self::getEmoteStatistics('moon2S', $db);
-			$angryStats = self::getEmoteStatistics('moon2A', $db);
-			$neutStats = self::getEmoteStatistics('moon2N', $db);
-			$pogChampStats = self::getEmoteStatistics('PogChamp', $db);
+			$stmt = $db->query("SELECT MAX(timestamp) FROM emotes");
+			$latestTimestamp = $stmt->fetch()[0];
+			
+			$shownMinutes = $request->query->get('shownMinutes', 5 * 60);
+			$earliestTimestamp = $latestTimestamp - $shownMinutes * 60 * 1000;
+
+			$mlemStats = self::getEmoteStatistics('moon2MLEM', $db, $earliestTimestamp);
+			$smileStats = self::getEmoteStatistics('moon2S', $db, $earliestTimestamp);
+			$angryStats = self::getEmoteStatistics('moon2A', $db, $earliestTimestamp);
+			$neutStats = self::getEmoteStatistics('moon2N', $db, $earliestTimestamp);
+			$pogChampStats = self::getEmoteStatistics('PogChamp', $db, $earliestTimestamp);
 
 			return $app['twig']->render('index.twig', [
 				'emotes' => $emotes,
+				'shownMinutes' => $shownMinutes,
 				'mlemStats' => $mlemStats,
 				'smileStats' => $smileStats,
 				'angryStats' => $angryStats,
@@ -34,28 +44,37 @@ class MainController implements ControllerProviderInterface
 				'pogChampStats' => $pogChampStats]);
 		})->bind('index');
 
+		/**
+		 * Emote Leaderboard
+		 */
 		$route->get('/emote/{emote}', function($emote) use($app, $db) {
-			$stmt = $db->query("SELECT * FROM emotes WHERE emote='$emote'");
-			$stats = $stmt->fetchAll();
-
-			$stmt = $db->query("SELECT DISTINCT username FROM emotes WHERE emote='$emote'");
-			$users = [];
-			while ($row = $stmt->fetch())
-				$users[] = $row[0];
-			sort($users);
+			$stmt = $db->query("SELECT username, MAX(occurrences) AS occurrences FROM emotes WHERE emote='$emote' GROUP BY username ORDER BY MAX(occurrences) DESC");
+			$leaderboard = $stmt->fetchAll();
 
 			return $app['twig']->render('emote.twig', [
 				'emote' => $emote,
-				'stats' => $stats,
-				'users' => $users]);
+				'leaderboard' => $leaderboard]);
 		})->bind('emote');
+
+		/**
+		 * Per-user stats for an emote
+		 */
+		$route->get('/emote/{emote}/user/{username}', function($emote, $username) use($app, $db) {
+			$stmt = $db->query("SELECT timestamp, occurrences FROM emotes WHERE emote='$emote' AND username='$username' ORDER BY timestamp ASC");
+			$stats = $stmt->fetchAll();
+			
+			return $app['twig']->render('user.twig', [
+				'emote' => $emote,
+				'username' => $username,
+				'stats' => $stats]);
+		})->bind('emote_user');
 
 		return $route;
 	}
 
 	// Returns array of (timestamp, occurrences) tuples sorted by timestamp for this emote
-	static function getEmoteStatistics($emote, $db) {
-		$stmt = $db->query("SELECT DISTINCT timestamp FROM emotes WHERE emote='$emote' ORDER BY timestamp ASC");
+	static function getEmoteStatistics($emote, $db, $earliestTimestamp=0) {
+		$stmt = $db->query("SELECT DISTINCT timestamp FROM emotes WHERE emote='$emote' AND timestamp >= $earliestTimestamp ORDER BY timestamp ASC");
 		$stats = [];
 		while ($row = $stmt->fetch())
 		{
