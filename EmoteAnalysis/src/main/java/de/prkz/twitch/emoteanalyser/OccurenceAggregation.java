6231c1ea0dfa4a2ff8fd3de13068d19d5209db39
vaehx <prosicraft@googlemail.com>
2018-07-02 02:01:22 +0200

    Fix timestamp not set

--- EmoteAnalysis/src/main/java/de/prkz/twitch/emoteanalyser/OccurenceAggregation.java
+++ EmoteAnalysis/src/main/java/de/prkz/twitch/emoteanalyser/OccurenceAggregation.java
@@ -50,6 +50,7 @@ public class OccurenceAggregation
 			occurences.occurrences++;
 
 		// Emit current occurrence count
+		occurences.timestamp = System.currentTimeMillis();
 		collector.collect(occurences);
 
 		LOG.info("user: " + occurences.username + ", emote: " + occurences.emote + ", occurences: " + occurences.occurrences);
