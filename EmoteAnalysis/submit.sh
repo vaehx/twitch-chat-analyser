d7b796486f16bc76093444b1abc27d5d6687d92c
vaehx <prosicraft@googlemail.com>
2018-07-04 12:15:38 +0200

    Increase parallelism in submit script to 4

--- EmoteAnalysis/submit.sh
+++ EmoteAnalysis/submit.sh
@@ -2,4 +2,4 @@
 
 docker cp ./target/emote-analyzer-1.0-SNAPSHOT.jar flink-jobmanager:/analyser.jar
 
-docker exec -ti flink-jobmanager flink run -c de.prkz.twitch.emoteanalyser.EmoteAnalyser /analyser.jar
+docker exec -ti flink-jobmanager flink run -p 4 -c de.prkz.twitch.emoteanalyser.EmoteAnalyser /analyser.jar
