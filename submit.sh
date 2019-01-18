4785bcaa0465e69e72f8a8729a79329c0417e372
vaehx <prosicraft@googlemail.com>
2019-01-18 01:52:06 +0100

    Avoid error message in submit script when checkpoints dir exists

--- submit.sh
+++ submit.sh
@@ -2,10 +2,10 @@
 
 docker cp ./target/emote-analyzer-1.0-SNAPSHOT.jar flink-jobmanager:/analyser.jar
 
-docker exec -u root flink-jobmanager mkdir /data/checkpoints
+docker exec -u root flink-jobmanager mkdir -p /data/checkpoints
 docker exec -u root flink-jobmanager chown flink:flink /data/checkpoints
 
-docker exec -u root flink-taskmanager mkdir /data/checkpoints
+docker exec -u root flink-taskmanager mkdir -p /data/checkpoints
 docker exec -u root flink-taskmanager chown flink:flink /data/checkpoints
 
 docker exec -ti flink-jobmanager flink run -p 1 -c de.prkz.twitch.emoteanalyser.EmoteAnalyser /analyser.jar \
