3daa26685447c4cfacbb913b70248577d105c94a
vaehx <prosicraft@googlemail.com>
2019-01-18 16:56:09 +0100

    Unify naming of docker containers

--- submit.sh
+++ submit.sh
@@ -2,12 +2,12 @@
 
 docker cp ./target/emote-analyzer-1.0-SNAPSHOT.jar flink-jobmanager:/analyser.jar
 
-docker exec -u root flink-jobmanager mkdir -p /data/checkpoints
-docker exec -u root flink-jobmanager chown flink:flink /data/checkpoints
+docker exec -u root tca_flink-jobmanager mkdir -p /data/checkpoints
+docker exec -u root tca_flink-jobmanager chown flink:flink /data/checkpoints
 
-docker exec -u root flink-taskmanager mkdir -p /data/checkpoints
-docker exec -u root flink-taskmanager chown flink:flink /data/checkpoints
+docker exec -u root tca_flink-taskmanager mkdir -p /data/checkpoints
+docker exec -u root tca_flink-taskmanager chown flink:flink /data/checkpoints
 
-docker exec -ti flink-jobmanager flink run -p 1 -c de.prkz.twitch.emoteanalyser.EmoteAnalyser /analyser.jar \
+docker exec -ti tca_flink-jobmanager flink run -p 1 -c de.prkz.twitch.emoteanalyser.EmoteAnalyser /analyser.jar \
 	"jdbc:postgresql://db:5432/twitch?user=postgres&password=password" \
 	"kafka:9092"
