7fe90769ae01a61e5bbc2c3af3b55157a5f0404a
vaehx <prosicraft@googlemail.com>
2022-10-19 15:32:04 +0200

    Modernize docker compose setup

--- submit.py
+++ submit.py
@@ -2,7 +2,15 @@
 
 import subprocess
 
-PARALLELISM = 1
+PARALLELISM = 4
+
+JAR_FILE = './build/libs/twitch-chat-analyzer-SNAPSHOT-all.jar'
+MAIN_CLASS = 'de.prkz.twitch.emoteanalyser.EmoteAnalyser'
+PROPERTIES_FILE = './job.properties'
+JOBMANAGER_SERVICE = 'flink-jobmanager'
+
+# in container
+CHECKPOINTS_DIR = '/data/checkpoints'
 
 
 # Runs shell command, e.g. ['ls', '-l'] and redirect process stdout to console stdout
@@ -12,15 +20,18 @@ def run_shell_command(command_arr):
 
 
 print('Copying jar into container...')
-run_shell_command(['docker', 'cp', './build/libs/twitch-chat-analyzer-1.1-SNAPSHOT-all.jar', 'tca_flink-jobmanager:/analyser.jar'])
+run_shell_command(['docker', 'compose', 'cp', JAR_FILE, JOBMANAGER_SERVICE + ':/analyser.jar'])
 
 print('Copying config into container...')
-run_shell_command(['docker', 'cp', './job.properties', 'tca_flink-jobmanager:/analyser.properties'])
+run_shell_command(['docker', 'compose', 'cp', PROPERTIES_FILE, JOBMANAGER_SERVICE + ':/analyser.properties'])
 
 print('Fixing permissions of data directories in container...')
-run_shell_command(['docker', 'exec', '-u', 'root', 'tca_flink-jobmanager', 'mkdir', '-p', '/data/checkpoints'])
-run_shell_command(['docker', 'exec', '-u', 'root', 'tca_flink-jobmanager', 'chown', 'flink:flink', '/data/checkpoints'])
+run_shell_command(['docker', 'compose' 'exec', '-T', '-u', 'root', JOBMANAGER_SERVICE, 'mkdir', '-p', CHECKPOINTS_DIR])
+run_shell_command(['docker', 'compose' 'exec', '-T', '-u', 'root', JOBMANAGER_SERVICE, 'chown', 'flink:flink', CHECKPOINTS_DIR])
 
 print('Submitting job...')
-run_shell_command(['docker', 'exec', 'tca_flink-jobmanager', 'flink', 'run', '-d', '-p', str(PARALLELISM), '-c', 'de.prkz.twitch.emoteanalyser.EmoteAnalyser', '/analyser.jar',
-    '/analyser.properties'])
+run_shell_command(['docker', 'compose', 'exec', '-T', JOBMANAGER_SERVICE, 'flink', 'run',
+                   '-d',
+                   '-p', str(PARALLELISM),
+                   '-c', MAIN_CLASS, '/analyser.jar',
+                   '/analyser.properties'])
